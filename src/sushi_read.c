ba880c5b96335d29b755efa3caa29c8587c3af58
TTBOSS333 <mr.patrickpengchao@yahoo.com>
2021-02-16 19:49:53 -0500

    wang_test

--- src/sushi_read.c
+++ src/sushi_read.c
@@ -4,14 +4,14 @@ static char *history[SUSHI_HISTORY_LENGTH] = {NULL};
 
 char *sushi_read_line(FILE *in) {
   /*
-    pengchao wang
+    pengchao_wang_test
    */
   return NULL;
 }
 
 int sushi_read_config(char *fname, int ok_if_missing) {
   /*
-    pengchao wang
+    pengchao_wang_test
    */
   return 0;
 }
