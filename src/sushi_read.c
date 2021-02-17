6e1c9d96449b857721aae59919b9d4abf7b0968b
TTBOSS333 <mr.patrickpengchao@yahoo.com>
2021-02-16 19:53:17 -0500

    Update sushi_read.c

--- src/sushi_read.c
+++ src/sushi_read.c
@@ -1,17 +1,22 @@
+  /*
+    pengchao_wang working on it 02/16/2021
+   */
+
+
 #include "sushi.h"
 
 static char *history[SUSHI_HISTORY_LENGTH] = {NULL};
 
 char *sushi_read_line(FILE *in) {
   /*
-    pengchao_wang_test
+    Your code goes here.
    */
   return NULL;
 }
 
 int sushi_read_config(char *fname, int ok_if_missing) {
   /*
-    pengchao_wang_test
+    Your code goes here.
    */
   return 0;
 }
