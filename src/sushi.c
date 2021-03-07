115f97e17879f3d36bc1092291190a215e8c3c89
shad-ee <78453794+shad-ee@users.noreply.github.com>
2021-03-06 22:54:32 -0500

    Update sushi.c

--- src/sushi.c
+++ src/sushi.c
@@ -9,15 +9,23 @@ int main() {
   if (sushi_read_config("sushi.conf", 1))
     return EXIT_FAILURE;
 
+
+while (sushi_exit == 0)
+{
   char *line;
 
   printf("%s", SUSHI_DEFAULT_PROMPT);
 
   if (!(line = sushi_read_line(stdin)))
+    sushi_parse_command(line);
     return EXIT_FAILURE;
 
   sushi_store(line);
+}
+
+  
   sushi_show_history();
   
   return EXIT_SUCCESS;
+
 }
